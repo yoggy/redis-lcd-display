c613c08093d40b970b978289a325d77d830feeef
yoggy <yoggy0@gmail.com>
2015-11-04 19:44:05 +0900

    modified:   raspi-status.rb

--- sample/raspi-status.rb
+++ sample/raspi-status.rb
@@ -18,9 +18,9 @@ loop do
   t_str += count % 2 == 0 ? ":" : " "
   t_str += t.strftime("%M")
   
-  r.set "lcd:0", `hostname`.chomp + " status"
+  r.set "lcd:0", `hostname -s`.chomp + " status"
   r.set "lcd:1", t_str
-  r.set "lcd:2", `LANG=C /sbin/ifconfig | grep -v 127.0.0.1 | grep inet | awk '{print $2}' | sed -e 's/addr://'`
+  r.set "lcd:2", `LANG=C /sbin/ifconfig | grep -v 127.0.0.1 | grep 'inet ' | awk '{print $2}' | sed -e 's/addr://'`
   
   count += 1
   sleep 1
